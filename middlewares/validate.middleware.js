d1b767272c6b7bf85644581e27e0cada0a6221bb
starling-king <jigal6518@gmail.com>
2026-08-31 19:32:51 +0530

    refactor validation middleware and user routes to enhance error handling and input validation

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,18 +1,26 @@
 import { ApiError } from "../error/ApiErrors.error.js";
 
 export const validate = (schema, source = "body") => (req, res, next) => {
+    try {
+        const result = schema.safeParse(req[source]);
 
-   
-    const result = schema.safeParse(req[source]);
+        if (result.success === true) {
+            req[source] = result.data;
+            return next(); 
+        }
 
-    if (result.success) {
-        
-        req[source] = result.data;
-        return next();
+        // const errorMessage = result.error.issues[0]?.message || "Invalid input data";
+
+        const errorMessage =
+            result.error.issues?.[0]?.message ||
+            result.error.errors?.[0]?.message ||
+            "Invalid input data";
+
+        return next(new ApiError(400, errorMessage));
+
+    } catch (error) {
+        console.error("VALIDATION MIDDLEWARE CRASHED:", error);
+
+        return next(new ApiError(500, "Validation middleware crashed"));
     }
-
-    const errorField = result.error.errors[0]?.path[0];
-    const errorMessage = result.error.errors[0]?.message || "Invalid request data";
-
-    throw new ApiError(400, `${errorField ? errorField + ' - ' : ''}${errorMessage}`);
 };
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,21 +1,22 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAdminDetails} from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
+import { validate } from "../middlewares/validate.middleware.js"
+import {
+    registerSchema,loginSchema,updateAdminSchema,changePasswordSchema} from "../validators/user.validator.js";
 
 const router = Router()
 
-router.route("/register").post(
-    registerUser
-)
+router.route("/register").post(validate(registerSchema),registerUser)
 
-router.route("/login").post(loginUser)
+router.route("/login").post(validate(loginSchema),loginUser)
 router.route("/refreshAccessToken").post(refreshAccessToken)
 
 //secure section
 router.route("/logout").post(verifyJwt,logoutUser)
-router.route("/changePassword").post(verifyJwt,changeCurrentPassword)
+router.route("/changePassword").post(verifyJwt,validate(changePasswordSchema),changeCurrentPassword)
 router.route("/getCurrentUser").get(verifyJwt,getCurrentUser)
-router.route("/updateAdminDetails").post(verifyJwt,updateAdminDetails)
+router.route("/updateAdminDetails").post(verifyJwt,validate(updateAdminSchema),updateAdminDetails)
 export default router
 
 
